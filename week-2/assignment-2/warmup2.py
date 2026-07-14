ebbc024521cef5bfa357dc8feb94ed5b126884ba
John Martin <jjoseph0575@gmail.com>
2026-07-13 22:25:44 -0400

    Fixed navigation in warmup2

--- week-2/assignment-2/warmup2.py
+++ week-2/assignment-2/warmup2.py
@@ -1,7 +1,6 @@
 # cd Documents/Workspace/Code the Dream/python-intro-homework
-# cd week-2/assignment-2
 # ls
-# python3 warmup2.py
+# cd week-2/assignment-2
 date_input = input("What is today's date? ")
 
 print(f"You said today is {date_input}.")
