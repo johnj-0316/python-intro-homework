139318e1383d4d878cba95e4b87ccfb819ae7e17
John Martin <jjoseph0575@gmail.com>
2026-07-13 22:27:43 -0400

    added warmup3 git log --oneline full output

--- week-2/assignment-2/warmup3.py
+++ week-2/assignment-2/warmup3.py
@@ -1,4 +1,26 @@
 # git log --oneline output:
 # b4984b9 (HEAD -> assignment-2) added warmup files 1-3, 3 empty
+# 294742d (origin/main, origin/HEAD, main) add week 2 folder
+# 58d836d Add files via upload
+# d8c0c05 Update README.md
+# a888c4b Delete week-5/data/.gitkeep
+# 06c7f75 Delete week-4/data/.gitkeep
+# 202444e Add files via upload
+# 677fee9 Add files via upload
+# ff7d9dc Add files via upload
+# e0ff870 Delete week-7/data/.gitkeep
+# c3876b0 Add files via upload
+# 30299f8 Merge pull request #1 from Code-the-Dream-School/add-gitkeeps
+# bd6befc foldering
+# 5f7a204 Create .gitkeep
+# d0a5396 Create .gitkeep
+# 545c44d Delete students.csv
+# 19f098e Delete notes.txt
+# 66766d2 Delete expenses.csv
+# a69c42e foldering
+# c0de231 Add files via upload
+# c487413 create folders & update instructions
+# 8b36eac Update README.md
+# 2eaa21f Initial commit
 
-print("I learned how to fork, use git switch -c, and git log --oneline!")
+print("I learned how to fork, use git switch -c, and git log --oneline this week!")
