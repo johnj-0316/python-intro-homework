3b9d2bb1e139dda97b8e389a218568eadcc38169
John Martin <jjoseph0575@gmail.com>
2026-07-20 21:57:52 -0400

    Replaced first ifs on work flow blocks to explicitly handle 0 first

--- week-3/assignment-3/warmup4.py
+++ week-3/assignment-3/warmup4.py
@@ -1,17 +1,17 @@
 num_input = input("Enter a number: ")
 num = int(num_input)
 
-if num > 0:
-    print(f"{num} is positive.")
+if num == 0:
+    print(f"{num} is zero.")
 elif num < 0:
     print(f"{num} is negative.")
 else:
-    print(f"{num} is zero.")
+    print(f"{num} is positive.")
     
-if num % 2:
-    print(f"{num} is odd.")
+if num == 0:
+    print(f"{num} is even.")
 elif not num % 2:
     print(f"{num} is even.")
 else:
-    print(f"{num} is even.")
+    print(f"{num} is odd.")
 
