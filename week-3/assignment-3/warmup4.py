2516fda333a879e6a8435cac5aba7aa3f1df468f
John Martin <jjoseph0575@gmail.com>
2026-07-20 21:43:28 -0400

    Commit initial week 3 work files (warmup 1-4 + mini_project)

---
+++ week-3/assignment-3/warmup4.py
@@ -0,0 +1,15 @@
+num_input = input("Enter a number: ")
+num = int(num_input)
+
+if num > 0:
+    print(f"{num} is positive.")
+elif num < 0:
+    print(f"{num} is negative.")
+else:
+    print(f"{num} is zero.")
+    
+if num % 2:
+    print(f"{num} is odd.")
+else:
+    print(f"{num} is even.")
+
