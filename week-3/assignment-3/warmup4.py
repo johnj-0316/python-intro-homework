fbedfe667bf23408f61b0b37103df71427598ee6
John Martin <jjoseph0575@gmail.com>
2026-07-20 22:02:31 -0400

    Fixed 0 to zero

--- week-3/assignment-3/warmup4.py
+++ week-3/assignment-3/warmup4.py
@@ -4,7 +4,7 @@ num = int(num_input)
 if num < 0:
     print(f"{num} is negative.")
 elif num == 0:
-    print(f"{num} is 0.")
+    print(f"{num} is zero.")
 else:
     print(f"{num} is positive.")
     
