fcde62cfa4daf22dfc3433c80043aac5bd1f063d
John Martin <jjoseph0575@gmail.com>
2026-07-27 20:19:18 -0400

    added labels to print statements in warmup3

--- week-4/assignment-4/warmup3.py
+++ week-4/assignment-4/warmup3.py
@@ -4,6 +4,6 @@ language_list_2 = ["Rust", "JavaScript", "SQL", "Go"]
 language_set_1 = set(language_list_1)
 language_set_2 = set(language_list_2)
 
-print(language_set_1 | language_set_2)
-print(language_set_1 & language_set_2)
-print(language_set_1 - language_set_2)
+print(f"Union: {language_set_1 | language_set_2}")
+print(f"Intersection: {language_set_1 & language_set_2}")
+print(f"Difference: {language_set_1 - language_set_2}")
