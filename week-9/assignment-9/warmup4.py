9c7cbffcf6d8179eea3cbd145e0c8672e4fe3aa7
John Martin <jjoseph0575@gmail.com>
2026-08-31 19:55:11 -0400

    finished warmup4.py

---
+++ week-9/assignment-9/warmup4.py
@@ -0,0 +1,10 @@
+import requests
+
+try:
+    res = requests.get("https://thisurldoesnotexist.example.com")
+    
+    if res.status_code != 200:
+        raise requests.exceptions.RequestException()
+
+except requests.exceptions.RequestException as request_error:
+    print("Error: Could not reach the server. Check your connection and try again.")
