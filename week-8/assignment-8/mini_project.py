11510b343b66f9990bc32b74fccc26005d576815
John Martin <jjoseph0575@gmail.com>
2026-08-31 12:06:48 -0400

    fixed error file message

--- week-8/assignment-8/mini_project.py
+++ week-8/assignment-8/mini_project.py
@@ -40,7 +40,7 @@ Clean data:
 {clean_data}
 """)            
     except FileNotFoundError as e:
-        print("There was an error opening the csv file.")
+        print("The csv file was not found in the data directory. Please check to make sure the file exists.")
         return
     
 if __name__ == "__main__":
