0ebc16c64687c80d27a58c904046d17a418608fb
John Martin <jjoseph0575@gmail.com>
2026-09-07 19:39:30 -0400

    it2

--- week-9/assignment-9/warmup4.py
+++ week-9/assignment-9/warmup4.py
@@ -1,10 +1,15 @@
 import requests
 
-try:
-    res = requests.get("https://thisurldoesnotexist.example.com")
-    
-    if res.status_code != 200:
+def main():
+    try:
+        res = requests.get("https://thisurldoesnotexist.example.com")
+        
+        if res.status_code != 200:
+            print("Error: Could not reach the server. Check your connection and try again.")
+            return
+
+    except requests.exceptions.RequestException as request_error:
         print("Error: Could not reach the server. Check your connection and try again.")
-    
-except requests.exceptions.RequestException as request_error:
-    print("Error: Could not reach the server. Check your connection and try again.")
+        
+if __name__ == "__main__":
+    main()
