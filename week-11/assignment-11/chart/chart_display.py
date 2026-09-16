5ce085b5a851dac6379eaf45a3a8195582e6ac5b
John Martin <jjoseph0575@gmail.com>
2026-09-15 22:46:16 -0400

    finished chart_display function

---
+++ week-11/assignment-11/chart/chart_display.py
@@ -0,0 +1,11 @@
+from matplotlib import pyplot
+
+def chart_display(title: str, categories: list[str], quantities: list[int]):
+    figure, axis = pyplot.subplots(figsize=(7, 5))
+    bars = axis.bar(categories, quantities, color='#2171B5', edgecolor='black', width=0.6)
+    axis.bar_label(bars, padding=3)
+    axis.set_title(title, fontsize=14, fontweight='bold', pad=15)
+    axis.set_xlabel('Slot Types', fontsize=12)
+    axis.set_ylabel('Slot Frequencies', fontsize=12)
+    pyplot.tight_layout()
+    pyplot.show()
